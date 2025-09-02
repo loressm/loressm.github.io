bdd0a7e6ab0b385c2cc4aca5bd327c62ee8c2431
loressm <lorerome90@virgilio.it>
2025-09-02 20:17:00 +0200

    Delete footer.js

--- footer.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    fetch('footer.html')  // Carica il file footer.html dalla root
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('footer').innerHTML = data;  // Inietta il contenuto nel footer
-        })
-        .catch(error => console.error('Errore nel caricare il footer:', error));
-};
