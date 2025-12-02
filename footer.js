92d31b75b32c980ea7519d8f7abc0ae857621fac
loressm <lorerome90@virgilio.it>
2025-12-02 15:54:47 +0100

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,3 +1,10 @@
-fetch("footer.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("footer").innerHTML = data);
+// Carica dinamicamente il footer
+document.addEventListener("DOMContentLoaded", () => {
+  const footerElement = document.getElementById("footer");
+  if (footerElement) {
+    fetch("footer.html")
+      .then(res => res.text())
+      .then(data => footerElement.innerHTML = data)
+      .catch(err => console.error("Errore nel caricamento del footer:", err));
+  }
+});
